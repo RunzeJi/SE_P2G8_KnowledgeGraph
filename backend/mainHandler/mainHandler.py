8dc159ffa23fff3265ced73dc17681264e0701a4
Runze Ji <jirunzeamerica@icloud.com>
2023-11-06 23:42:27 +0800

    Update Login Logic
    Added login handler code, passes username and password to Python -- 添加登录逻辑，将用户名和密码传入Python

--- backend/mainHandler/mainHandler.py
+++ backend/mainHandler/mainHandler.py
@@ -5,16 +5,27 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return 
+    return render_template('login.html')
+
+@app.route('/login_clicked', methods=['POST'])
+def login_clicked():
+    username = request.form.get('username_input')
+    password = request.form.get('password_input')
+    print(username)
+    print(password)
+    return render_template('signupHandler.html')
 
 @app.route('/signup')
 def signup():
-    return
+    return render_template('signup.html')
 
-@app.home('/home')
+@app.route('/home')
 def home():
-    return 
+    return render_template('home.html')
 
 @app.route('/root')
 def root():
-    return
+    return render_template('root.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
